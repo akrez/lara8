62bc2f2be9164650c3e2b440deab865495510898
Akrez <akrez.like@gmail.com>
2022-04-11 23:19:00 +0430

    seprate app and blank

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -26,9 +26,38 @@
     @yield('POS_HEAD')
 </head>
 
-<body class="login-page">
+<body class="hold-transition sidebar-mini layout-fixed rtl">
     @yield('POS_BEGIN')
-    @yield('content')
+    <div id="app" class="wrapper">
+        <!-- Navbar -->
+        @include('layouts.navbar')
+        <!-- Main Sidebar Container -->
+        @include('layouts.sidebar')
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid ">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">@yield('content-header')</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            @yield('content-breadcrumb')
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.content-header -->
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </section>
+        </div>
+        <!-- /.content-wrapper -->
+        @include('layouts.footer')
+    </div>
     <!-- ./wrapper -->
     <!-- jQuery -->
     <script src="{{asset('js/jquery.js')}}"></script>

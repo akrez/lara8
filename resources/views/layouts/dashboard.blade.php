2b44aca8c2caf00629756dddf570169d105a1f0e
Akrez <akrez.like@gmail.com>
2022-04-21 18:41:17 +0430

    dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -10,19 +10,17 @@
     <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
     <title>@yield('title', config('app.name'))</title>
     <!-- Font Sahel -->
-    <link rel="stylesheet" href="{{asset('css/font-sahel.css')}}">
-    <!-- Font SahelFD -->
-    <link rel="stylesheet" href="{{asset('css/font-sahel-fd.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/vazirmatn-v32.102/Vazirmatn-font-face.css')}}">
     <!-- All -->
-    <link rel="stylesheet" href="{{asset('css/fontawesome-all.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/fontawesome-free-5.15.4-web/css/all.min.css')}}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('css/adminlte.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/AdminLTE-3.2.0/css/adminlte.css')}}">
     <!-- Theme RTL style -->
     <link rel="stylesheet" href="{{asset('css/adminlte-rtl.css')}}">
     <!-- bootstrap-rtl -->
     <link rel="stylesheet" href="{{asset('css/bootstrap-rtl.css')}}">
     <!-- style -->
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/dashboard.css')}}">
     @yield('POS_HEAD')
 </head>
 
@@ -60,19 +58,19 @@
     </div>
     <!-- ./wrapper -->
     <!-- jQuery -->
-    <script src="{{asset('js/jquery.js')}}"></script>
+    <script src="{{asset('dist/jquery-3.6.0/jquery.min.js')}}"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="{{asset('js/jquery-ui.js')}}"></script>
+    <script src="{{asset('dist/jquery-ui-1.13.1/jquery-ui.min.js')}}"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+    <script src="{{asset('dist/bootstrap-4.6.1-dist/js/bootstrap.bundle.min.js')}}"></script>
     <!-- AdminLTE App -->
-    <script src="{{asset('js/adminlte.js')}}"></script>
+    <script src="{{asset('dist/AdminLTE-3.2.0/js/adminlte.min.js')}}"></script>
     <!-- script -->
-    <script src="{{asset('js/script.js')}}"></script>
+    <script src="{{asset('js/dashboard.js')}}"></script>
 
     @yield('POS_END')
 </body>
